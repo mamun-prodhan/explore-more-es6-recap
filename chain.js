15359bb836138663bba94e0b6906d8fc74232e07
mamun-prodhan <mamunpr9@gmail.com>
2023-08-24 17:19:54 +0600

    first commit

---
+++ chain.js
@@ -0,0 +1,14 @@
+const user = {
+  id: 5001,
+  name: "shoriful Raj",
+  address: {
+    street: {
+      fist: "uttor side",
+      second: "poribag er goli",
+      third: "no dorai",
+    },
+    city:'Dhaka'
+  },
+};
+
+console.log(user.address.street.second);

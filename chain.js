de507a33dbd242afe1d6d528f23988c3200ff467
mamun-prodhan <mamunpr9@gmail.com>
2023-08-24 17:48:39 +0600

    chain

--- chain.js
+++ chain.js
@@ -10,5 +10,13 @@ const user = {
     city:'Dhaka'
   },
 };
+const user2 = {
+  id: 5001,
+  name: "shoriful Raj",
+  address: {
+    city:'Dhaka'
+  },
+};
 
 console.log(user.address.street.second);
+console.log(user2.address?.street?.second);

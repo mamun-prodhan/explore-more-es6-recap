0cf2b353bdb742f712f51df72d0db0d0c7bf32eb
mamun-prodhan <mamunpr9@gmail.com>
2023-08-24 18:59:01 +0600

    filter

---
+++ filter.js
@@ -0,0 +1,3 @@
+const friends = ['arjun', 'mamun', 'shohagh khan', 'mehedi', 'arif'];
+const filtered = friends.filter(friend => friend.length <= 5);
+console.log(filtered);
---
+++ forEach.js
@@ -0,0 +1,5 @@
+const friends = ['arjun', 'mamun', 'shohagh khan', 'mehedi', 'arif'];
+
+const result = friends.forEach(friend => friend);
+
+console.log(result);

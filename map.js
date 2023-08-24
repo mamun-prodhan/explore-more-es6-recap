9b786ece5938e40080567041c8fdb88a0ae769d1
mamun-prodhan <mamunpr9@gmail.com>
2023-08-24 17:53:10 +0600

    map.js

---
+++ map.js
@@ -0,0 +1,5 @@
+const friends = ['arjun', 'mamun', 'shohagh khan', 'mehedi', 'arif'];
+
+// const length = friends.map(friend => friend.length);
+const length = friends.map(friend => friend[0]);
+console.log(length)
